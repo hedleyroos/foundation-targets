e645a7b222fa4d47c90d60a500df4fd7a167b111
hedleyroos <hedleyroos@gmail.com>
2011-07-04 12:46:45 +0200

    Modelling

--- src/foundationtargets/foundationtargets/admin.py
+++ src/foundationtargets/foundationtargets/admin.py
@@ -1,2 +1,21 @@
 from django.contrib import admin
 
+from foundationtargets import models
+
+class GeckoWidgetAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+class DataPointInline(admin.TabularInline):
+    model = models.DataPoint
+    extra = 6
+
+class DeterminantAdmin(admin.ModelAdmin):
+    list_display = ('title', 'gecko_widget')
+    inlines = (DataPointInline,)
+
+class DataPointAdmin(admin.ModelAdmin):
+    list_display = ('determinant', 'month', 'target', 'actual')
+
+admin.site.register(models.GeckoWidget, GeckoWidgetAdmin)
+admin.site.register(models.Determinant, DeterminantAdmin)
+admin.site.register(models.DataPoint, DataPointAdmin)
--- src/foundationtargets/foundationtargets/models.py
+++ src/foundationtargets/foundationtargets/models.py
@@ -1,1 +1,36 @@
+from django.db import models
+
+class GeckoWidget(models.Model):
+    title = models.CharField(max_length=128)    
+
+    class Meta:
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Determinant(models.Model):
+    title = models.CharField(max_length=128)
+    gecko_widget = models.ForeignKey(GeckoWidget)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return self.title
+
+
+class DataPoint(models.Model):
+    determinant = models.ForeignKey(Determinant)
+    month = models.DateField()
+    target = models.PositiveIntegerField()
+    actual = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('determinant', 'month',)
+        unique_together = ('determinant', 'month')
+
+    def __unicode__(self):
+        return '%s > %s' % (self.determinant.title, self.month.strftime('%B %Y'))
 
