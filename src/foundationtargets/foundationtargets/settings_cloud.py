d08b6f580cef3800a1c784d5f6acbe04b11c713f
hedleyroos <hedleyroos@gmail.com>
2011-07-05 20:07:31 +0200

    Postgres needs host setting

--- src/foundationtargets/foundationtargets/settings_cloud.py
+++ src/foundationtargets/foundationtargets/settings_cloud.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': 'foundationtargets', # Or path to database file if using sqlite3.
         'USER': 'foundationtargets', # Not used with sqlite3.
         'PASSWORD': 'foundationtargets', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
     }
 }
