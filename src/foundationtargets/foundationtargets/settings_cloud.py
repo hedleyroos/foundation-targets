55f47d6769435d88369b066f5688a2bd497d64aa
hedleyroos <hedleyroos@gmail.com>
2011-07-05 20:03:22 +0200

    Add cloud settings

---
+++ src/foundationtargets/foundationtargets/settings_cloud.py
@@ -0,0 +1,13 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'foundationtargets', # Or path to database file if using sqlite3.
+        'USER': 'foundationtargets', # Not used with sqlite3.
+        'PASSWORD': 'foundationtargets', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
